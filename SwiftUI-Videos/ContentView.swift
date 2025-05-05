2640f1119faf42540d450425e5a41014519a62d5
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-05 16:35:37 +0630

    update UI

--- SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/ContentView.swift
@@ -1,24 +1,46 @@
-//
-//  ContentView.swift
-//  SwiftUI-Videos
-//
-//  Created by Digital Base on 5/5/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    var videos: [Video] = VideoList.topTwelve
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView {
+            List(videos) { video in
+                
+                HStack{
+                    Image(video.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 90)
+                        .cornerRadius(8)
+                    
+                    Spacer()
+                        .frame(width: 16)
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(video.title)
+                            .fontWeight(.semibold)
+                            .lineLimit(2)
+                            .minimumScaleFactor(0.75)
+                        
+                        Spacer().frame(height: 5)
+                        
+                        Text(video.uploadDate)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }.navigationBarTitle("Videos")
+            }
         }
-        .padding()
     }
 }
 
+
+
 #Preview {
-    ContentView()
+    ContentView(videos:  VideoList.topTwelve)
 }
+
